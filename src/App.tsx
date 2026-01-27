95ff8c24226291bc3f369cf8ebfd9ffb7d0903c8
Veli-Pekka Nurmi <nurmi.vp@gmail.com>
2026-01-27 18:18:05 +0200

    Removed obsolite component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { ThemeProvider } from "styled-components";
 import theme from "./common/theme";
 import Layout from "./components/Layout";
 
-import Main from "./pages/Main";
 import About from "./pages/About";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <Layout>
-        {/* <Main /> */}
         <About />
       </Layout>
     </ThemeProvider>
